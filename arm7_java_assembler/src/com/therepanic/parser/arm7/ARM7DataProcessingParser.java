b8393ca698550e94c195b3092141357d777dbb80
Andrey Litvitski <andrey1010102008@gmail.com>
2025-10-06 21:53:46 +0300

    add support for comparison suffixes

--- arm7_java_assembler/src/com/therepanic/parser/arm7/ARM7DataProcessingParser.java
+++ arm7_java_assembler/src/com/therepanic/parser/arm7/ARM7DataProcessingParser.java
@@ -26,12 +26,46 @@ public class ARM7DataProcessingParser extends ARM7Parser {
             Map.entry("MVN", 0b1111)
     );
 
+    private static final Map<String, Integer> COND_CODES = Map.ofEntries(
+            Map.entry("EQ", 0b0000),
+            Map.entry("NE", 0b0001),
+            Map.entry("CS", 0b0010),
+            Map.entry("CC", 0b0011),
+            Map.entry("MI", 0b0100),
+            Map.entry("PL", 0b0101),
+            Map.entry("VS", 0b0110),
+            Map.entry("VC", 0b0111),
+            Map.entry("HI", 0b1000),
+            Map.entry("LS", 0b1001),
+            Map.entry("GE", 0b1010),
+            Map.entry("LT", 0b1011),
+            Map.entry("GT", 0b1100),
+            Map.entry("LE", 0b1101)
+    );
+
     @Override
     public Instruction parse(List<String> tokens) {
         String op = tokens.getFirst();
+        int opCode = OP_CODES.get(op.substring(0, 3));
+        int condCode;
+        if (op.length() > 3) {
+            condCode = COND_CODES.get(op.substring(3));
+        } else {
+            //AL cond code
+            condCode = 0b1110;
+        }
+        for (var opCodeEntry : OP_CODES.entrySet()) {
+            if (op.startsWith(opCodeEntry.getKey())) {
+                opCode = opCodeEntry.getValue();
+                if (op.length() > 3) {
+                    condCode = COND_CODES.get(op.substring(3));
+                }
+                break;
+            }
+        }
         int instruction = 0;
-        instruction |= 0b1110 << 28;
-        instruction |= OP_CODES.get(op) << 21;
+        instruction |= condCode << 28;
+        instruction |= opCode << 21;
         if ("CMP".equals(op) || "CMN".equals(op) || "TST".equals(op) || "TEQ".equals(op)) {
             instruction |= 1 << 20;
         } else {
